caaece86341b72863d6c95b67cf3d1241bf4f891
NitroGuy10 <naviyer10@gmail.com>
2022-03-25 22:23:12 -0500

    Slightly better buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+let plugin = require("tailwindcss/plugin")
+
 module.exports = {
   content: ["./pages/index.html"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant("grayed-out", ".grayed-out")
+    })
+  ]
 }
